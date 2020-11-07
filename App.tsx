0cda9b74873457c5b1b3cfd28d33493c91fc7b5f
Bruno Costa <euobrunocosta@gmail.com>
2020-11-07 07:45:19 -0300

    - Added the Orders tab and screen
    - Added icons to the tabs

--- App.tsx
+++ App.tsx
@@ -4,13 +4,29 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ProductsScreen from './src/screens/Products/List'
 import ClientsScreen from './src/screens/Clients/List'
+import OrdersScreen from './src/screens/Orders/List'
+import { AntDesign, Octicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            switch (route.name) {
+              case 'Orders':
+                return <Octicons name="package" size={24} color={color} />
+              case 'Products':
+                return <AntDesign name="shoppingcart" size={24} color={color} />
+              case 'Clients':
+                return <AntDesign name="contacts" size={24} color={color} />
+            }
+          },
+        })}
+      >
+        <Tab.Screen name="Orders" component={OrdersScreen} />
         <Tab.Screen name="Products" component={ProductsScreen} />
         <Tab.Screen name="Clients" component={ClientsScreen} />
       </Tab.Navigator>
---
+++ src/screens/Orders/List.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const OrdersListScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Orders List Screen</Text>
+    </View>
+  )
+}
+
+export default OrdersListScreen
